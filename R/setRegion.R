ce1908794d57f093eaa78a8eb4d77f8dba59d4b6
Vince Carey <stvjc@channing.harvard.edu>
2008-08-08 11:03:28 +0000

    various
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyContam@33135 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/setRegion.R
+++ R/setRegion.R
@@ -1,6 +1,7 @@
 
 setRectRegion = function(x, chip=1, xinds = 251:350, yinds = 251:350, vals = 10,
-  cdfname = "hgu133plus2cdf", valgen=NULL) {
+   valgen=NULL) {
+  cdfname = paste(annotation(x), "cdf", sep="")
   require(cdfname, character.only=TRUE, quietly=TRUE)
   badco = expand.grid(xinds, yinds)
   indsbad = apply(badco,1,function(x) xy2indices(x[1], x[2], 
@@ -11,7 +12,8 @@ setRectRegion = function(x, chip=1, xinds = 251:350, yinds = 251:350, vals = 10,
 }
 
 setCircRegion = function(x, chip=1, center=c(350,350), rad=100,
-  vals = 10, cdfname = "hgu133plus2cdf", valgen=NULL) {
+  vals = 10, valgen=NULL) {
+  cdfname = paste(annotation(x), "cdf", sep="")
   require(cdfname, character.only=TRUE, quietly=TRUE)
   xext = seq(center[1]-rad, center[1]+rad)
   yext = seq(center[2]-rad, center[2]+rad)
@@ -22,4 +24,25 @@ setCircRegion = function(x, chip=1, center=c(350,350), rad=100,
   if (is.null(valgen)) exprs(x)[indsbad, chip] = vals
   else exprs(x)[indsbad, chip] = valgen(length(indsbad))
   x
+}
+
+getCircRegion = function(x, chip=1, center=c(350,350), rad=100) {
+  cdfname = paste(annotation(x), "cdf", sep="")
+  require(cdfname, character.only=TRUE, quietly=TRUE)
+  xext = seq(center[1]-rad, center[1]+rad)
+  yext = seq(center[2]-rad, center[2]+rad)
+  badco = expand.grid(xext, yext)
+  badco = badco[ (badco[,1] - center[1])^2 + (badco[,2]-center[2])^2 < rad^2, ]
+  indsbad = apply(badco,1,function(x) xy2indices(x[1], x[2], 
+    cdf=cdfname))
+  return(as.numeric(exprs(x)[indsbad, chip]))
+}
+
+getRectRegion = function(x, chip=1, xinds = 251:350, yinds = 251:350) {
+  cdfname = paste(annotation(x), "cdf", sep="")
+  require(cdfname, character.only=TRUE, quietly=TRUE)
+  badco = expand.grid(xinds, yinds)
+  indsbad = apply(badco,1,function(x) xy2indices(x[1], x[2], 
+    cdf=cdfname))
+  return(as.numeric(exprs(x)[indsbad, chip]))
 }
